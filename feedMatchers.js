9221ac9c7b31b219cc6c5124a23d2d4ae8eed9b7
Charlie Root <root@thisguy.website>
2018-01-11 22:50:15 -0500

    Adding Dragon Ball Super to RSS Feed

--- feedMatchers.js
+++ feedMatchers.js
@@ -9,5 +9,14 @@ exports.feedMatchers = [
         loader: function(item) {
             addTorrent(item.link, 'videos/Shows/One Piece');
         }
+    },
+    {
+        url: 'http://horriblesubs.info/rss.php?res=1080',
+        fileRegexes: [
+            /^\[HorribleSubs\] Dragon Ball Super - /
+        ],
+        loader: function(item) {
+            addTorrent(item.link, 'videos/Shows/Dragon Ball Super');
+        }
     }
 ];
